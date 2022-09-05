e464be7f190e88fd7133e0c7a4bce29041142960
Juan Perdomo <juanj.perdomov@uqvirtual.edu.co>
2022-09-05 17:12:42 -0500

    Primer commit

---
+++ negocio/src/main/java/co/edu/uniquindio/unicine/NegocioApplication.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.unicine;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class NegocioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NegocioApplication.class, args);
+    }
+}
